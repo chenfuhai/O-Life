86f607d686ec41da27ef6057675076b548fd9819
chenfuhai <1169555653@qq.com>
2017-06-03 18:48:04 +0800

    试验了volley

---
+++ test/src/main/java/com/olife/test/MainActivity.java
@@ -0,0 +1,38 @@
+package com.olife.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView tv= (TextView) findViewById(R.id.text);
+        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest stringRequest = new StringRequest(NetConfig.TestAction, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                tv.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                tv.setText(error.getMessage());
+            }
+        });
+
+        mQueue.add(stringRequest);
+
+    }
+}
---
+++ test/src/main/java/com/olife/test/NetConfig.java
@@ -0,0 +1,15 @@
+package com.olife.test;
+/**
+ * ÍøÂç·şÎñÆ÷IPÅäÖÃ
+ * @author chenfuhai
+ *
+ */
+public class NetConfig {
+	public static String IP="http://10.100.0.100";
+	public static String PORT="1433";
+	public static String DateBaseName="olife";
+	public static String USERNAME="sa";
+	public static String PASSWORD="fdc159";
+	public static String HTTPPORT="8080";
+	public static String TestAction = IP+":"+HTTPPORT+"/olifeManager/test.action";
+}
