27cfc0e21e263d991c5ed1c6482222074f3c8e76
chenfuhai <1169555653@qq.com>
2017-06-04 18:53:39 +0800

    写了一个HTTP 工具类 这个类可以上传JSON  和文件

--- test/src/main/java/com/olife/test/MainActivity.java
+++ test/src/main/java/com/olife/test/MainActivity.java
@@ -1,14 +1,8 @@
 package com.olife.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,22 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView tv= (TextView) findViewById(R.id.text);
-        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
 
-        StringRequest stringRequest = new StringRequest(NetConfig.TestAction, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                tv.setText(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-                tv.setText(error.getMessage());
-            }
-        });
-
-        mQueue.add(stringRequest);
-
+        String JSON ="{'abc6':'123','abc5':'123','abc4':'123','abc3':'123','abc2':'123','abc1':'123'}";
+        HttpUntils.getInstance().postwithJSON(NetConfig.TestAction, JSON,
+                new HttpUntils.SuccessListener() {
+                    @Override
+                    public void onSuccessResponse(String result) {
+                        tv.setText(result);
+                    }
+                }, new HttpUntils.FailedListener() {
+                    @Override
+                    public void onFialed(int connectCode) {
+                        tv.setText(connectCode);
+                    }
+                }
+        );
     }
 }
