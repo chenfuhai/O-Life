a3b228f006df35a4a0c05376a1d012ced3b74e08
chenfuhai <1169555653@qq.com>
2017-06-12 14:15:21 +0800

    ...

--- app/src/main/java/com/olife/o_life/entity/Goods.java
+++ app/src/main/java/com/olife/o_life/entity/Goods.java
@@ -9,6 +9,15 @@ public class Goods {
     private String iconurl;
     private String name;
     private String desc;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public int getId() {
         return id;
